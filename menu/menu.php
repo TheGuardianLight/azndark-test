1edc4e71ec791a24aebaa36b65380649901a075a
TheLi <thelightguardian@outlook.fr>
2021-12-03 16:12:47 +0100

    Ajout page "notre-chaîne-twitch" et modification du menu

--- menu/menu.php
+++ menu/menu.php
@@ -4,7 +4,7 @@
         <ul>
             <li><a href="/index.php" target="_self">Accueil</a></li>
             <li><a href="" target="_self" onclick="alert('Cette page est encore en construction !')">Nos r&eacute;seaux sociaux</a></li>
-            <li><a href="" target="_self" onclick="alert('Cette page est encore en construction !')">Twitch</a></li>
+            <li><a href="/notre-chaine-twitch.php" target="_self">Notre cha&icirc;ne Twitch</a></li>
             <li class="deroulant"><a href="" target="_self">Plus</a>
                 <ul class="sous">
                     <li><a href="" onclick="alert('Cette page est encore en construction !')">Contact</a></li>
