aad4ce1fc8a37b3be2da146001eed8eb687ffb28
TheLi <thelightguardian@outlook.fr>
2021-12-04 17:53:37 +0100

    Finition des mentions légales + ajouts des logos Copyright.

--- mention-legales.php
+++ mention-legales.php
@@ -62,8 +62,65 @@
         <div>
             <h2 class="titre-2">Contact du propri&eacute;taire du contenu&nbsp;:</h2>
             <p><span class="underline">Team&nbsp;:</span> AznDarkProduction</p>
+            <p class="indication" id="1">&sup1;Est considéré comme contenu&nbsp;:</p>
+            <ul style="margin-left: 1.5vw">
+                <li class="listColor"><p class="indication descContenu">Toutes images repr&eacute;sentant le logo de la AznDarkProduction.</p></li>
+                <li class="listColor"><p class="indication descContenu">Toutes vid&eacute;o originale propos&eacute; par la AznDarkProduction par le biais de ce site.</p> </li>
+            </ul>
         </div>
-
+        <hr/>
+        <div>
+            <h2 class="titre-2">Contact du support du site&nbsp;:</h2>
+            <p><span class="underline">Email&nbsp;:</span> <a href="mailto:support@thelightguardian.fr">support@thelightguardian.fr</a></p>
+        </div>
+        <hr/>
+        <div>
+            <h2 class="titre-2">H&eacute;bergeur Web&nbsp;:</h2>
+            <p>Site web et nom de domaine h&eacute;berg&eacute; par&nbsp;: <strong><a href="https://www.ovh.com/">OVHCloud</a></strong></p>
+            <p>Si&egrave;ge social&nbsp;: <strong>2 rue kellermann - 59100 Roubaix - France</strong></p>
+            <p><a href="https://www.ovh.com/fr/support/mentions-legales/">Voir les mentions l&eacute;gales d'OVHCloud</a></p>
+        </div>
+        <hr/>
+        <div>
+            <!-- Description de la license copyright utilisé -->
+            <h2 class="titre-3">Licence Copyright et droits d'auteurs</h2>
+            <p style="text-align: center"><a rel="license" href="https://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licence Creative Commons" class="license-generale-cc" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-nd.png" /></a><br />Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="https://creativecommons.org/licenses/by-nc-nd/4.0/">Licence Creative Commons Attribution - Pas d&#39;Utilisation Commerciale - Pas de Modification 4.0 International</a>.</p>
+            <p style="text-align: center;">Un résumé de la licence <span class="underline">BY-NC-ND 4.0</span> est disponible <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.fr">ici</a>.</p>
+            <p style="text-align: center;">En vertu de cette même licence, vous êtes tenu de respecter les mêmes règles que celle-ci, à savoir&nbsp;:</p>
+        </div>
+        <hr/>
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <img alt="Copyright BY" src="styles/images/logo_copyright/by.xlarge.webp" class="copyrightImg copyImg1">
+                    </td>
+                    <td>
+                        <img alt="Copyright BY" src="styles/images/logo_copyright/by.xlarge.webp" class="copyrightImg copyImg2">
+                        <p><strong>Attribution (BY)</strong> — Vous devez créditer l'Œuvre, intégrer un lien vers la licence et indiquer si des modifications ont été effectuées à l'Oeuvre. Vous devez indiquer ces informations par tous les moyens raisonnables, sans toutefois suggérer que l'Offrant vous soutient ou soutient la façon dont vous avez utilisé son Oeuvre.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img alt="Copyright NC" src="styles/images/logo_copyright/nc-eu.xlarge.webp" class="copyrightImg copyImg1">
+                    </td>
+                    <td>
+                        <img alt="Copyright BY" src="styles/images/logo_copyright/nc-eu.xlarge.webp" class="copyrightImg copyImg2">
+                        <p><strong>Pas d’Utilisation Commerciale (NC)</strong> — Vous n'êtes pas autorisé à faire un usage commercial de cette Œuvre, tout ou partie du matériel la composant.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img alt="Copyright ND" src="styles/images/logo_copyright/nd.xlarge.webp" class="copyrightImg copyImg1"/>
+                    </td>
+                    <td>
+                        <img alt="Copyright BY" src="styles/images/logo_copyright/lnd.xlarge.webp" class="copyrightImg copyImg2">
+                        <p><strong>Pas de modifications (ND)</strong> — Dans le cas où vous effectuez un remix, que vous transformez, ou créez à partir du matériel composant l'Œuvre originale, vous n'êtes pas autorisé à distribuer ou mettre à disposition l'Œuvre modifiée.</p>
+                    </td>
+                </tr>
+            </table>
+            <p style="text-align: center;"><strong>Pas de restrictions complémentaires</strong> — Vous n'êtes pas autorisé à appliquer des conditions légales ou des mesures techniques qui restreindraient légalement autrui à utiliser l'Œuvre dans les conditions décrites par la licence.</p>
+        </div>
     </div>
 
 </div>
--- menu/menu.php
+++ menu/menu.php
@@ -7,8 +7,8 @@
             <li><a href="/notre-chaine-twitch.php" target="_self">Notre cha&icirc;ne Twitch</a></li>
             <li class="deroulant"><a href="" target="_self">Plus</a>
                 <ul class="sous">
-                    <li><a href="" onclick="alert('Cette page est encore en construction !')">Contact</a></li>
-                    <li><a href="/mention-legales.php">Mention L&eacute;gales</a></li>
+                    <li><a href="" target="_self" onclick="alert('Cette page est encore en construction !')">Contact</a></li>
+                    <li><a href="/mention-legales.php" target="_self">Mention L&eacute;gales</a></li>
                 </ul>
             </li>
         </ul>
